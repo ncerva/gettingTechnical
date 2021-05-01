f0160c3b8484b6c85f2f5713ec3b6c71de4bb6c8
Nayeli Cervantes <nayelicervantes40@gmail.com>
2021-05-01 18:27:17 -0500

    models done

--- models/index.js
+++ models/index.js
@@ -2,12 +2,29 @@ const Title = require('./Title');
 const User = require('./User');
 const Post = require('./Post');
 
-Gallery.hasMany(Painting, {
-foreignKey: 'gallery_id',
+Title.belongsTo(User, {
+foreignKey: 'user_id',
 });
 
-Painting.belongsTo(Gallery, {
-foreignKey: 'gallery_id',
+User.hasMany(Title, {
+foreignKey: 'user_id',
 });
 
-module.exports = { User, Gallery, Painting };
+Post.belongsTo(User, {
+foreignKey: 'user_id',
+});
+
+Post.belongsTo(Title, {
+foreignKey: 'user_id',
+});
+
+User.hasMany(Post, {
+foreignKey: 'user_id',
+});
+
+Title.hasMany(Post, {
+foreignKey: 'post_id',
+onDelete: "cascade"
+});
+
+module.exports = { Title, User, Post };
